4a05f3b6e7b2d4d79b57f33b45b0d9113959b564
Shivani-Indla <shiva.indla1518@gmail.com>
2025-04-27 13:16:57 +0530

    My 3rd commit in shell-script for shell scripting

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,15 @@
-testing the code.
+#!/bin/bash
+
+PERSON1="Shiva"
+PERSON2="Sravani"
+
+echo "$PERSON1:: Hello $PERSON2 how are you?"
+echo "$PERSON2:: Hello $PERSON1 i am good. How are you?"
+echo "$PERSON1:: I am also good $PERSON2. Thanks for asking."
+echo "$PERSON2:: I am glad to hear that $PERSON1."
+echo "$PERSON1:: I am going to sleep now. Good night $PERSON2."
+echo "$PERSON2:: Good night $PERSON1. Sweet dreams."
+echo "$PERSON1:: Good night $PERSON2. Sweet dreams."
+echo "$PERSON1:: I am going to sleep now. Good night."
+echo "$PERSON2:: Good night $PERSON1. Sweet dreams."
+echo "$PERSON1:: Good night $PERSON2. Sweet dreams."
