6805324c0c332fe70c5d9235b146a74f5c12d7f4
Shivani-Indla <shiva.indla1518@gmail.com>
2025-04-27 22:48:25 +0530

    My 13th commit in shell-script for scripting

--- 09-instalations.sh
+++ 09-instalations.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-if [ UID -eq 0 ];
+if [ $UID -eq 0 ];
 then
-    dnf install -y mysql
+    dnf install -y nginx
 else
     echo "Please run this script as root or with sudo."
-    exit 1
 fi
