ec9dda603130953c9d59a35919baeb9964a456eb
Shivani-Indla <shiva.indla1518@gmail.com>
2025-04-27 14:57:14 +0530

    My 6th commit in shell-script for scripting

--- 04-pass-secret-passwd.sh
+++ 04-pass-secret-passwd.sh
@@ -4,4 +4,12 @@ echo "Please enter username::"
 read USERNAME    #here USRNAME is a variable
 echo "Please enter password::"
 read -s PASSWORD   # -s option is used to hide the password while typing
+echo "Please enter confirm password::"
+read -s CONFIRM_PASSWORD
+if [ "$PASSWORD" == "$CONFIRM_PASSWORD" ]; 
+then
+    echo "Password matched"
+else
+    echo "Password not matched"
+fi
 echo "Username is $USERNAME and password is $PASSWORD"
