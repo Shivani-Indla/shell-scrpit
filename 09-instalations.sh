bf29beeb79a723163abbe018792a58912a53cf51
Shivani-Indla <shiva.indla1518@gmail.com>
2025-04-27 22:59:58 +0530

    My 13th commit in shell-script for scripting

--- 09-instalations.sh
+++ 09-instalations.sh
@@ -2,7 +2,9 @@
 
 if [ $UID -eq 0 ];
 then
-    dnf install -y nginx
+    echo "You are root user."
 else
     echo "Please run this script as root or with sudo."
 fi
+    dnf install -y nginx
+    echo "Nginx installed successfully."
