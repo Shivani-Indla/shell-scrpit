adf822734f334f4b079596a45e3ca84950df0600
Shivani-Indla <shiva.indla1518@gmail.com>
2025-04-27 19:22:22 +0530

    My 11th commit in shell-script for scripting

--- 07-spl-vars.sh
+++ 07-spl-vars.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-echo "Printing all variables: $@"
-echo "Printing all variables: $*"
-echo "Printing all variables: $?"
-echo "Printing all variables: $#"
-echo "Printing all variables: $-"
-echo "Printing all variables: $!"
-echo "Printing all variables: $$"
-echo "Printing all variables: $0"
-echo "Printing all variables: $1"
-echo "Printing all variables: $2"
-echo "Printing all variables: $3"
+echo "Printing all variables: $@" #printing all variables
+echo "Printing all variables: $*" #printing all variables
+echo "printing last command status: $?" #printing last command status(0=success, 1=error)
+echo "printing number of arguments passed to script: $#" #printing number of arguments passed to script
+echo "printing all options set for the shell: $-" #printing all options set for the shell
+echo "printing last background process id: $!" #printing last background process id
+echo "PID of current shell: $$" #PID of current shell
+echo "name of the script: $0" #name of the script
+echo "first argument passed to script: $1" #first argument passed to script
+echo "second argument passed to script: $2" #second argument passed to script
