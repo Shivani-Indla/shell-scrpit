83ef2037db1e0c50d221d9164ef2d5db93da96a9
Shivani-Indla <shiva.indla1518@gmail.com>
2025-04-27 20:09:24 +0530

    My 12th commit in shell-script for scripting

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -5,7 +5,8 @@ NUMBER=$1  #pass the argument from command line # ex: ./08-conditions.sh 10
 if [ $NUMBER -gt 10 ]; 
 then
     echo "Given number $NUMBER is grater than 10"
-elif [ $NUMBER -lt 0 ]; then
+elif [ $NUMBER -lt 0 ]; 
+then
     echo "Given number $NUMBER is less than 10"
 else
     echo "$NUMBER is zero"
