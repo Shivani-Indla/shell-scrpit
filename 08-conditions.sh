8a2ba541f64cea79302c932e1337151bd6bdb525
Shivani-Indla <shiva.indla1518@gmail.com>
2025-04-27 22:42:18 +0530

    My 13th commit in shell-script for scripting

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -2,7 +2,13 @@
 
 NUMBER=$1  #pass the argument from command line # ex: ./08-conditions.sh 10
 
-if [ $NUMBER -gt 10 ]; 
+if [ $NUMBER -gt 10 ]; # -gt, -lt, -eq, -ne, -ge, -le are used for comparing numbers
+then
+    echo "Given number $NUMBER is greater than 10"
+elif [ $NUMBER -eq 10 ]; 
+then
+    echo "Given number $NUMBER is equal to 10"
+elif [ $NUMBER -ge 10 ];
 then
     echo "Given number $NUMBER is grater than 10"
 elif [ $NUMBER -lt 10 ]; 
