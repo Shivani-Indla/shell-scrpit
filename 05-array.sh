c0dbcf7272266997974eec59f57709b5e41a169a
Shivani-Indla <shiva.indla1518@gmail.com>
2025-04-27 15:11:02 +0530

    My 7th commit in shell-script for scripting

---
+++ 05-array.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+MOVIES( "RRR", "KGF", "Bahubali", "Dangal" )   #array
+
+echo "first movie is ${MOVIES[0]}"
+echo "second movie is ${MOVIES[1]}"     
+echo "third movie is ${MOVIES[2]}"
+echo "fourth movie is ${MOVIES[3]}"
+echo "total number of movies are ${#MOVIES[@]}"  #length of array
+echo "all movies are ${MOVIES[@]}"  #all elements of array
+echo "all movies are ${!MOVIES[@]}"  #all indexes of array
+echo "all movies are ${MOVIES[*]}"  #all elements of array
+echo "all movies are ${!MOVIES[*]}"  #all indexes of array
+echo "all movies are ${MOVIES[@]:1:2}"  #slicing of array
+echo "all movies are ${MOVIES[@]:0:2}"  #slicing of array
+echo "all movies are ${MOVIES[@]:2:2}"  #slicing of array
+echo "all movies are ${MOVIES[@]:3:2}"  #slicing of array
+echo "all movies are ${MOVIES[@]:4:2}"  #slicing of array
+echo "all movies are ${MOVIES[@]:5:2}"  #slicing of array
+echo "all movies are ${MOVIES[@]:6:2}"  #slicing of array
+echo "all movies are ${MOVIES[@]:7:2}"  #slicing of array
+echo "all movies are ${MOVIES[@]:8:2}"  #slicing of array
+echo "all movies are ${MOVIES[@]:9:2}"  #slicing of array
