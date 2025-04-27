1c67d3315304c924651ac0a82a221cce56420c8c
Shivani-Indla <shiva.indla1518@gmail.com>
2025-04-27 19:12:14 +0530

    My 11th commit in shell-script for scripting

---
+++ 07-spl-vars.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Printing all variables: $@"
+echo "Printing all variables: $*"
+echo "Printing all variables: $?"
+echo "Printing all variables: $#"
+echo "Printing all variables: $-"
+echo "Printing all variables: $!"
+echo "Printing all variables: $$"
+echo "Printing all variables: $0"
+echo "Printing all variables: $1"
+echo "Printing all variables: $2"
+echo "Printing all variables: $3"
