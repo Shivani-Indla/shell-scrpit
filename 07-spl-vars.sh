39bea8411b7017f28b6ad36ad1c66255236f972d
Shivani-Indla <shiva.indla1518@gmail.com>
2025-04-27 19:34:29 +0530

    My 11th commit in shell-script for scripting

--- 07-spl-vars.sh
+++ 07-spl-vars.sh
@@ -4,6 +4,18 @@ echo "Printing all variables: $@" #printing all variables
 echo "Printing all variables: $*" #printing all variables
 echo "printing last command status: $?" #printing last command status(0=success, 1=error)
 echo "printing number of arguments passed to script: $#" #printing number of arguments passed to script
+echo "printing present working directory: $PWD" #printing present working directory
+echo "printing home directory: $HOME" #printing home directory
+echo "printing current shell: $SHELL" #printing current shell
+echo "printing current shell name: $BASH" #printing current shell name
+echo "printing current shell version: $BASH_VERSION" #printing current shell version
+echo "printing current shell process id: $BASHPID" #printing current shell process id
+echo "printing current shell process id: $PPID" #printing current shell process id
+echo "printing current shell process id: $OSTYPE" #printing current shell process id
+echo "printing current shell process id: $UID" #printing current shell process id
+echo "printing current shell process id: $HOSTNAME" #printing current shell process id
+echo "printing current shell process id: $PS1" #printing current shell process id
+echo "printing current shell process id: $PS2" #printing current shell process id
 echo "printing all options set for the shell: $-" #printing all options set for the shell
 echo "printing last background process id: $!" #printing last background process id
 echo "PID of current shell: $$" #PID of current shell
